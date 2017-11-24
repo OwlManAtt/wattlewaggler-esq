26475b53432e843ef5b164542f2ce169352bd604
Nick Evans <nicholas.evans@northwestern.edu>
2017-11-24 14:17:13 -0600

    Add logo to brand spot on nav

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -13,7 +13,10 @@
         <header>
            <!-- Fixed navbar -->
            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-             <a class="navbar-brand" href="/">Wattlewaggler &amp; Associates</a>
+             <a class="navbar-brand" href="/">
+                 <img src="{{ asset('/image/logo.png') }}" alt="">
+                 <span class="text-hide">Wattlewaggler &amp; Associates</span>
+             </a>
              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
              </button>
